deec164b6a261f9a994db436717613c40e2c5444
ChaeminKang-E <90402077+ChaeminKang-E@users.noreply.github.com>
2021-11-16 09:49:04 +0900

    Upload
    Upload

---
+++ justjs.js
@@ -0,0 +1,4 @@
+let temp=293
+temp-=273 //Kelvin to Celsius
+temp=temp*9/5+32 //Celsius to Fahrenheit
+console.log(Math.floor(temp))//Flooring temp value to makt it integer and then printing temp value
