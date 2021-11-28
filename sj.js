08c2d987c7235217d45e9c2ac0a2a0d356add9a0
ChaeminKang-E <90402077+ChaeminKang-E@users.noreply.github.com>
2021-11-28 11:11:54 +0900

    REMINDER TO ADD COMMIT TITLE
    Algorithms 1

---
+++ sj.js
@@ -0,0 +1,17 @@
+function add(a, b){
+    return a+b
+}
+function circ(n){
+    return n*Math.PI
+}
+function smaller(a, b){
+    return a+b>100?true:false
+}
+function leap(y){
+    return y%4==0?true:false
+}
+function ten(a, b){
+    return a==10||b==10||a+b==10?true:false
+}
+let XfromF //type =(any function name)(and parameters value) here
+console.log(XfromF)
